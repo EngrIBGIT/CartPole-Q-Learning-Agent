0dd9ca7083e244ac0da6fcc998f63252c449c540
EngrIBGIT <ibrahim5322022@hotmail.com>
2024-12-12 08:28:26 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
